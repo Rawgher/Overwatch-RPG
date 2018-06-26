8b69d70833f82a2db5039b07bee60758b034518d
Roger <rdpender3@gmail.com>
2018-06-25 23:14:17 -0700

    added jquery notes

--- assets/javascript/games.js
+++ assets/javascript/games.js
@@ -29,3 +29,15 @@ characters = [
     },
 ];
 
+// append characters into html (characters id)
+
+//change class on click of character
+//send other three characters to enemy character section (enemiesAvailable id)
+//change class or id of enemy chosen to move to defender zone (defender id)
+//set up fight (attack button)
+    // needs to subtract attack from characters hp
+    // needs to have main characters attack go up each time
+    // need to make character disappear when they die or go back up and put an x on them?
+    // need to set reset button on completion of game (remove hidden class)
+    // need to set lose alert
+    //need to set win alert
