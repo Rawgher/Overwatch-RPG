c9a4f95e25c2c75a837352a5ec1435f92c05a3d5
Roger <rdpender3@gmail.com>
2018-06-25 23:02:23 -0700

    making basic html and css

---
+++ assets/javascript/games.js
@@ -0,0 +1,31 @@
+characters = [
+    {
+        name = "Winston",
+        image= "assets/images/winston.jpg",
+        attack = 30,
+        counterAttack = 25,
+        health = 300,
+    },
+    {
+        name = "Reaper",
+        image= "assets/images/reaper.jpg",
+        attack = 20,
+        counterAttack = 35,
+        health = 200,
+    },
+    {
+        name = "Mercy",
+        image= "assets/images/mercy.jpg",
+        attack = 10,
+        counterAttack = 15,
+        health = 150,
+    },
+    {
+        name = "Reinhardt",
+        image= "assets/images/reinhardt.jpg",
+        attack = 15,
+        counterAttack = 15,
+        health = 400,
+    },
+];
+
